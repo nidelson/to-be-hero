9c60458e7e244b0f516e758158e5157ece0a479a
Nidelson Gimenes <nidelson.gimenes@upx.com>
2020-03-28 23:28:46 -0300

    Initial commit backend

---
+++ packages/backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3333;
+
+app.get('/', (request, response) => {
+  response.json({
+    message: 'Ping',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`[SERVER] Running at http://localhost:${PORT}`);
+});
